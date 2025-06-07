eed2dfd43b47815ba0a16fb7145b20e2725e3066
georg5ru <151039020+georg5ru@users.noreply.github.com>
2025-06-08 00:09:19 +0300

    Create Vehicle.java

---
+++ src/Vehicle.java
@@ -0,0 +1,15 @@
+public abstract class Vehicle {
+    protected String modelName;
+    protected int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public abstract void check();
+}
